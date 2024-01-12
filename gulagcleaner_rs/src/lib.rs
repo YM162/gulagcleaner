79088b36ed682f0aefcb72821bf2b0138fae1bf2
Alvaro <alvarocaboac2@gmail.com>
2024-01-12 15:50:26 +0100

    Fix docs

--- gulagcleaner_rs/src/lib.rs
+++ gulagcleaner_rs/src/lib.rs
@@ -1,12 +1,14 @@
 /*!
- *  [![github]](https://github.com/TuTarea/vinted-rs/)
- * [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
- *
- * # GulagCleaner crate in Rust
- * Gulag Cleaner is a tool designed to remove advertisements from PDFs, making it easier to read and navigate documents without being disrupted by unwanted ads.
- *
- * # Examples
- *
+
+ [![github]](https://github.com/YM162/gulagcleaner)
+
+  [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
+
+ # GulagCleaner crate in Rust
+ Gulag Cleaner is a tool designed to remove advertisements from PDFs, making it easier to read and navigate documents without being disrupted by unwanted ads.
+
+ # Examples
+
     ```rust
     use gulagcleaner_rs::clean::clean_pdf;
 
