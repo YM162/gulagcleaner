246f5e8435438a2be047768d626df868c2208b12
YM162 <david.fontaneda@estudiante.uam.es>
2024-01-16 16:11:44 +0100

    0.12.1

--- gulagcleaner_python/python/gulagcleaner/command_line.py
+++ gulagcleaner_python/python/gulagcleaner/command_line.py
@@ -61,7 +61,7 @@ def main():
 
     # Check for the -v argument
     if arguments["version"]:
-        print("Current version: 0.11.1")
+        print("Current version: 0.12.1")
         return
 
     # Get the pdf_path argument
