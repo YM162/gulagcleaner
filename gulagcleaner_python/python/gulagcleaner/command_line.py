e587ba7c6599138c646c27ff811bf1cc9250bda7
YM162 <david.fontaneda@YM162.dev>
2024-06-01 16:27:15 +0200

    Bumped version to 0.14.1

--- gulagcleaner_python/python/gulagcleaner/command_line.py
+++ gulagcleaner_python/python/gulagcleaner/command_line.py
@@ -61,7 +61,7 @@ def main():
 
     # Check for the -v argument
     if arguments["version"]:
-        print("Current version: 0.14.0")
+        print("Current version: 0.14.1")
         return
 
     # Get the pdf_path argument
